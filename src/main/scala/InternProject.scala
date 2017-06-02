59068676ab8c416081c5b4e4bf9476822878b310
konzy <bkonzman@gmail.com>
2017-06-02 11:18:12 -0400

    edited cql and queries

--- src/main/scala/InternProject.scala
+++ src/main/scala/InternProject.scala
@@ -1,12 +1,11 @@
 import org.apache.spark.sql.catalyst.expressions.Year
-import org.apache.spark.sql.{Column, DataFrame, SaveMode, SparkSession}
+import org.apache.spark.sql._
 import org.apache.spark.ml.classification.LogisticRegression
 import org.apache.spark.ml.feature.VectorAssembler
 import org.apache.spark.ml.linalg
 import org.apache.spark.ml.linalg.Vectors
 import org.apache.spark.sql.types._
 import org.apache.spark.sql.functions._
-import org.apache.spark.sql.Column
 
 /**
   * Created by bkonzman on 5/31/17.
@@ -42,39 +41,43 @@ object InternProject {
     val joinedDF = customersDataFrame.join(ordersDataFrame)
       .where(customersDataFrame("customer_id") === ordersDataFrame("customer_id"))
       .drop(ordersDataFrame("customer_id"))
-
     joinedDF.cache()
 
-    //    total amount spent by each customer
     val amountSpentAgg = joinedDF.select($"customer_id", $"amount").groupBy($"customer_id").sum("amount")
       .withColumnRenamed("sum(amount)", "sum_amount").sort($"sum_amount" desc).limit(10)
 
+    System.out.println("amount_spent_agg")
     amountSpentAgg.show
 
-    System.out.println("amount_spent_agg")
     saveToCassandra("amount_spent_agg", amountSpentAgg)
 
-//    customer ordering the most
 
-    // TODO: min max avg median
-    val mostOrderedCustomer = amountSpentAgg.select($"customer_id", $"sum_amount").groupBy($"customer_id").max("sum_amount")
-      .withColumnRenamed("max(sum_amount)", "max_sum_amount")
+    val aggregateSummary = joinedDF.select($"customer_id", $"amount")
+      .agg(
+        functions.count("customer_id").as("total_customers"),
+        functions.sum("amount").as("total_spent"),
+        functions.max("amount").as("max_spent"),
+        functions.min("amount").as("least_spent"),
+        functions.avg("amount").as("avg_spent"),
+        functions.mean("amount").as("mean_spent"),
+        functions.skewness("amount").as("skewness")
+      )
 
-    //mostOrderedCustomer.show
+    aggregateSummary.show()
 
-    System.out.println("customer_ordered_most")
-    saveToCassandra("customer_ordered_most", mostOrderedCustomer)
+    System.out.println("agg_summary")
+    saveToCassandra("agg_summary", aggregateSummary)
 
 
-//    state with the most customers
+    val mostCustomersByState = joinedDF.select($"state")
+      .groupBy($"state")
+      .count()
+      .sort($"count".desc)
 
-    // TODO: do by state sort by amount
-    val mostCustomersByState = joinedDF.select($"state").groupBy($"state").count().select(first("state"), max("count"))
-      .withColumnRenamed("max(count)", "max_count")
-      .withColumnRenamed("first(state, false)", "best_state")
+    mostCustomersByState.show()
 
-    System.out.println("most_customers_by_state")
-    saveToCassandra("most_customers_by_state", mostCustomersByState)
+    System.out.println("customers_by_state")
+    saveToCassandra("customers_by_state", mostCustomersByState)
 
     //    total sales in august
     val augTotalSales = ordersDataFrame.filter(month($"order_date").equalTo(8)).groupBy().sum("amount")
@@ -85,11 +88,12 @@ object InternProject {
 
 //    growth from 2016-17
     val totalSalesByMonth = joinedDF.groupBy(year($"order_date"), month($"order_date")).sum("amount")
-    .withColumnRenamed("year(order_date)", "year")
-    .withColumnRenamed("month(order_date)", "month")
-    .withColumnRenamed("sum(amount)", "total_sales")
+      .withColumnRenamed("year(order_date)", "year")
+      .withColumnRenamed("month(order_date)", "month")
+      .withColumnRenamed("sum(amount)", "total_sales")
+      .sort($"year", $"month")
 
-    //totalSalesByMonth.sortBy(r => (r.recent, r.freq))
+    totalSalesByMonth.show()
 
     System.out.println("total_sales_by_month")
     saveToCassandra("total_sales_by_month", totalSalesByMonth)
@@ -103,12 +107,11 @@ object InternProject {
 
     val sales = totalSalesByMonth.withColumnRenamed("total_sales", "labels").withColumn("features", ($"year" * 10) + $"month")
 
-    import spark.implicits._
     val assembler = new VectorAssembler()
       .setInputCols(Array("year", "month"))
       .setOutputCol("features")
 
-    sales.show
+    //sales.show
 
     val lr = new LogisticRegression()
       .setMaxIter(10)
@@ -116,10 +119,11 @@ object InternProject {
       .setElasticNetParam(0.8)
 
     // Fit the model
-    val lrModel = lr.fit(sales)
+
+    //val lrModel = lr.fit(sales)
 
     // Print the coefficients and intercept for logistic regression
-    println(s"Coefficients: ${lrModel.coefficients} Intercept: ${lrModel.intercept}")
+    //println(s"Coefficients: ${lrModel.coefficients} Intercept: ${lrModel.intercept}")
 //
 //    // We can also use the multinomial family for binary classification
 //    val mlr = new LogisticRegression()
