d721769a7a05b42a3e59bb07527621a5cc92871f
konzy <bkonzman@gmail.com>
2017-06-06 14:34:49 -0400

    ml stuff

--- src/main/scala/InternProject.scala
+++ src/main/scala/InternProject.scala
@@ -1,25 +1,39 @@
-import org.apache.spark.sql.catalyst.expressions.Year
 import org.apache.spark.sql._
-import org.apache.spark.ml.classification.LogisticRegression
-import org.apache.spark.ml.feature.VectorAssembler
-import org.apache.spark.ml.linalg
-import org.apache.spark.ml.linalg.Vectors
-import org.apache.spark.sql.types._
-import org.apache.spark.sql.functions._
 
 /**
-  * Created by bkonzman on 5/31/17.
+  * @author Brian Konzman
+  * This program creates several tables in a Cassandra database from mock sales data, namely order.csv and customer.csv
+  * These two tables are joined together and the resulting DataFrame is cached and used throughout the rest of the program
+  * to create 5 more tables.  These tables are created in memory locally then stored back to the cassandra database.
+  *
+  * amount_spent_agg
+  * total amount spent per customer over lifetime
+  *
+  * agg_summary
+  * gives several aggregations, max, min, mean, skew...
+  *
+  * customers_by_state
+  * gives the total customers by state
+  *
+  * total_sales_aug
+  * gives the total sales in augusts
+  *
+  * total_sales_by_month
+  * gives total sales by month and year
+  *
   */
+
 object InternProject {
   def main(args: Array[String]): Unit = {
     val spark: SparkSession = SparkSession
       .builder()
       .appName("test_company")
       .config("spark.master", "local")
-      .getOrCreate() //for fsql functions
+      .getOrCreate()
 
     import spark.implicits._ //for $ notation
 
+    //read data from mock csv customer
     val customersDataFrame = spark.read.format("com.databricks.spark.csv")
       .option("header", "true") //reading the headers
       .option("inferSchema", "true")
@@ -28,6 +42,7 @@ object InternProject {
 
     customersDataFrame.createOrReplaceTempView("customers")
 
+    //read from mock csv order
     val ordersDataFrame = spark.read.format("com.databricks.spark.csv")
       .option("header", "true") //reading the headers
       .option("inferSchema", "true")
@@ -38,21 +53,22 @@ object InternProject {
 
     spark.sqlContext.setConf("Cluster/spark.cassandra.connection.host", "localhost") //for example
 
+    //natural join on customers and order DataFrames
     val joinedDF = customersDataFrame.join(ordersDataFrame)
       .where(customersDataFrame("customer_id") === ordersDataFrame("customer_id"))
       .drop(ordersDataFrame("customer_id"))
     joinedDF.cache()
 
+    //Create DataFrame that gives the total amount spent per Customer
     val amountSpentAgg = joinedDF.select($"customer_id", $"amount").groupBy($"customer_id").sum("amount")
       .withColumnRenamed("sum(amount)", "sum_amount").sort($"sum_amount" desc)
 
-    System.out.println("amount_spent_agg")
-    amountSpentAgg.show
-
+    //save DataFrame to cassandra database
     saveToCassandra("amount_spent_agg", amountSpentAgg)
 
-    import org.apache.spark.sql.functions._
+    import org.apache.spark.sql.functions._ //allows you to use count() instead of functions.count()
 
+    //create DataFrame with many reduce aggregations
     val aggregateSummary = joinedDF.select($"customer_id", $"amount")
       .agg(
         count("customer_id").as("total_customers"),
@@ -64,30 +80,23 @@ object InternProject {
         skewness("amount").as("skewness")
       )
 
-    aggregateSummary.show()
-
-    System.out.println("agg_summary")
     saveToCassandra("agg_summary", aggregateSummary)
 
-
+    //create DataFrame with total customers by state
     val mostCustomersByState = joinedDF.select($"state")
       .groupBy($"state")
       .count()
       .sort($"count".desc)
 
-    mostCustomersByState.show()
-
-    System.out.println("customers_by_state")
     saveToCassandra("customers_by_state", mostCustomersByState)
 
-
+    //create DataFrame that gives the total amount ordered in dollars
     val augTotalSales = ordersDataFrame.filter(month($"order_date").equalTo(8)).groupBy().sum("amount")
     .withColumnRenamed("sum(amount)", "sum_amount")
 
-    System.out.println("total_sales_aug")
     saveToCassandra("total_sales_aug", augTotalSales)
 
-
+    //create DataFrame that gives total sales per month
     val totalSalesByMonth = joinedDF.groupBy(year($"order_date"), month($"order_date")).sum("amount")
       .withColumnRenamed("year(order_date)", "year")
       .withColumnRenamed("month(order_date)", "month")
@@ -95,22 +104,19 @@ object InternProject {
       .sort($"year", $"month")
       .withColumn("unique_id", monotonically_increasing_id())
 
-
-    totalSalesByMonth.show()
-    totalSalesByMonth.printSchema()
-
-    System.out.println("total_sales_by_month")
     saveToCassandra("total_sales_by_month", totalSalesByMonth)
-
-
-    totalSalesByMonth.createOrReplaceTempView("totalSales")
-    totalSalesByMonth.show()
-    totalSalesByMonth.printSchema()
 
     joinedDF.unpersist()
 
   }
 
+  /**
+    * This is a helper method for writing to our cassandra cluster.  Given a DataFrame and the name of the table
+    * this method will store the data in the already created cassandra table.
+    *
+    * @param nameOfTable
+    * @param table
+    */
   def saveToCassandra(nameOfTable: String, table: DataFrame): Unit = {
     table.write.format("org.apache.spark.sql.cassandra")
       .options(Map("cluster" -> "Cluster", "table" -> nameOfTable, "keyspace" -> "sales_data"))
--- src/main/scala/MachineLearningTest.scala
+++ src/main/scala/MachineLearningTest.scala
@@ -1,19 +1,17 @@
-import org.apache.spark.ml.classification.LogisticRegression
 import org.apache.spark.ml.feature.VectorAssembler
-import org.apache.spark.sql.{SaveMode, SparkSession}
-import com.datastax.spark.connector._
-import org.apache.spark.SparkContext
-import org.apache.spark.SparkContext._
-import org.apache.spark.SparkConf
-import com.datastax.spark.connector.cql.CassandraConnector
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.ml.clustering.KMeans
 import org.apache.spark.ml.regression.LinearRegression
 
 /**
-  * Created by bkonzman on 6/2/17.
+  * @author Brian Konzman
+  *
+  * This program loads data from the cassandra database and does a linear regression and a k-means clustering algorithm on it.
   */
 object MachineLearningTest {
   def main(args: Array[String]): Unit = {
 
+    //create spark session (context)
     val spark = SparkSession
       .builder()
       .appName("SparkCassandraApp")
@@ -23,29 +21,34 @@ object MachineLearningTest {
 
     import spark.implicits._ //for $ notation
 
+    //set our cassandra cluster to our spark session(context)
     spark.sqlContext.setConf("Cluster/spark.cassandra.connection.host", "localhost") //for example
 
+    //read data specified by the cluster, table, and keyspace into a DataFrame
     val totalSalesByMonth = spark.read.format("org.apache.spark.sql.cassandra")
       .options(Map("cluster" -> "Cluster", "table" -> "total_sales_by_month", "keyspace" -> "sales_data"))
       .load()
 
     totalSalesByMonth.createOrReplaceTempView("total_sales")
 
+    //create a new DataFrame with our total sales column renamed label
     val df = totalSalesByMonth.select("total_sales", "year", "month").withColumnRenamed("total_sales", "label")
-    df.show()
 
+    //create a new column with our features as a collection
     val assembler = new VectorAssembler()
       .setInputCols(Array("year", "month"))
       .setOutputCol("features")
 
+    //add the column and select only label and features (optional)
     val output = assembler.transform(df).select("label", "features")
     output.show()
 
     val lr = new LinearRegression()
 
-    // Fit the model to the data and call this model lrModel
+    // Fit the model to the data
     val lrModel = lr.fit(output)
 
+    println("year, month")
     println(s"Coefficients: ${lrModel.coefficients} Intercept: ${lrModel.intercept}")
 
     val trainingSummary = lrModel.summary
@@ -55,6 +58,19 @@ object MachineLearningTest {
     println(s"RMSE: ${trainingSummary.rootMeanSquaredError}")
     println(s"MSE: ${trainingSummary.meanSquaredError}")
     println(s"r2: ${trainingSummary.r2}")
+
+    //set our model to have 2 clusters
+    val kmeans = new KMeans().setK(2).setSeed(1L)
+    val model = kmeans.fit(output)
+
+    // Evaluate clustering by computing Within Set Sum of Squared Errors.
+    val WSSSE = model.computeCost(output)
+    println(s"Within Set Sum of Squared Errors = $WSSSE")
+
+    // Shows the result.
+    println("Cluster Centers: ")
+    model.clusterCenters.foreach(println)
+
   }
 
 }
