f676a7cb1a3eaa313f516e9cdad446d33d37c4d4
konzy <bkonzman@gmail.com>
2017-06-20 16:55:23 -0400

    pulling from the cassandra database now

--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,1 +1,3 @@
 addSbtPlugin("com.eed3si9n" % "sbt-assembly" % "0.13.0")
+
+resolvers += "Atlassian 3rd Party" at "http://maven.jahia.org/maven2/"
--- src/main/scala/SmartAccountProject.scala
+++ src/main/scala/SmartAccountProject.scala
@@ -7,10 +7,7 @@ import org.apache.spark.sql.types.StringType
   * Created by bkonzman on 6/19/17.
   */
 
-private val ORACLE_CONNECTION_URL: String = "jdbc:oracle:thin:@(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)" +
-  "(HOST=dbs-prd-vm-3034.cisco.com)(PORT=1551))(CONNECT_DATA=(SERVICE_NAME=EPICTRN.CISCO.COM)(Server=Dedicated))) "
-private val ORACLE_USERNAME: String = "user"
-private val ORACLE_PWD: String = "pwd"
+
 
 object SmartAccountProject {
   def main(args: Array[String]): Unit = {
@@ -24,20 +21,9 @@ object SmartAccountProject {
 
     import spark.implicits._ //for $ notation
 
-    val connectionProperties = new Properties()
-    connectionProperties.put("user", ORACLE_USERNAME)
-    connectionProperties.put("password", ORACLE_PWD)
+    //1.6.2
 
 
-    import org.apache.spark.sql.Dataset
-
-    val query = "select XXTMT.compressor.blob_decompress(tpv.pld) as \"UNCOMPRESSED_BLOB\", " +
-      "tpv.CREATE_DT,tpv.COMPRESSION_FORMAT from tmt_tran_log ttl, tmt_tran_stat_log ttsl, tmt_tran_stat_mst tsm,tmt_pld_ver tpv " +
-      "where ttl.tran_log_id=ttsl.tran_log_id and tsm.tran_stat_id=ttsl.tran_stat_id and ttl.tran_log_id=tpv.tran_log_id " +
-      "and ttsl.tran_stat_id=tpv.tran_stat_id and lower(tsm.tran_stat_nm)='order request received.' " +
-      "order by tpv.create_dt desc;"
-
-    val jdbcDF = spark.read.jdbc(ORACLE_CONNECTION_URL, query, "UNCOMPRESSED_BLOB", 10001, 499999, 10, connectionProperties)
 
 //    val jdbcDF = spark.read
 //      .format("jdbc")
@@ -47,22 +33,35 @@ object SmartAccountProject {
 //      .option("password", "password")
 //      .load()
 
-    val df = spark.sql("select XXTMT.compressor.blob_decompress(tpv.pld) as \"UNCOMPRESSED_BLOB\", " +
-      "tpv.CREATE_DT,tpv.COMPRESSION_FORMAT from tmt_tran_log ttl, tmt_tran_stat_log ttsl, tmt_tran_stat_mst tsm,tmt_pld_ver tpv " +
-      "where ttl.tran_log_id=ttsl.tran_log_id and tsm.tran_stat_id=ttsl.tran_stat_id and ttl.tran_log_id=tpv.tran_log_id " +
-      "and ttsl.tran_stat_id=tpv.tran_stat_id and lower(tsm.tran_stat_nm)='order request received.' " +
-      "order by tpv.create_dt desc;")
+
+
+
 
     import org.apache.spark.sql._
 
-    spark.sqlContext.setConf("Cluster/spark.cassandra.connection.host", "localhost") //for example
+    spark.sqlContext.setConf("Cluster/spark.cassandra.connection.host", "ccwa-cssdb-stg1-01")
+    spark.sqlContext.setConf("Cluster/spark.cassandra.auth.username", "oprepusr")
+    spark.sqlContext.setConf("Cluster/spark.cassandra.auth.password", "Rtp987")
+
+    val b2bXml = spark.sqlContext.read.format("org.apache.spark.sql.cassandra").options(Map("cluster" -> "Cluster", "keyspace"->"test", "table"->"b2b_xml_data")).load()
+
+
+
 
 
     //val df = spark.sql("select body from test limit 3"); // body is a json encoded blob column
-    val df2 = df.select(df("UNCOMPRESSED_BLOB").cast(StringType).as("XML"))
+    val df2 = b2bXml.select(b2bXml("UNCOMPRESSED_BLOB").cast(StringType).as("XML"))
 
-    val rdd = df2.rdd.map { case Row(j: String) => j }
-    spark.read.json(rdd).show()
+    df2.show()
+
+    val rdd = df2.rdd.map {
+      case Row(j: String) => j
+    }
+
+    rdd.foreach(println)
+
+
+//    spark.read.format("com.databricks.spark.xml").load(rdd)
 
 
 
